b68a27b979be6dc6192a9088f3bfb90bead076ab
Tatiana <Tatiana_novik@mail.ru>
2022-07-10 21:11:11 +0700

    Все задачи

--- Task2.py
+++ Task2.py
@@ -6,4 +6,5 @@ for X in True, False:
     for Y in True, False:
         for Z in True, False:
             result = result and (not(X or Y or Z)==(not X and not Y and not Z))
-            print(result)
+            print(f'X={X}   Y={Y}   Z={Z} -->   {result}')
+
